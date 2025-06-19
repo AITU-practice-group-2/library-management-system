48bbd45a7840961899f75508780b39016a27e5fb
Zhandos200 <dildahanz@mail.ru>
2025-06-19 12:17:01 +0500

    Adding logger

--- src/main/java/com/practice/librarysystem/review/ReviewController.java
+++ src/main/java/com/practice/librarysystem/review/ReviewController.java
@@ -1,14 +1,18 @@
 package com.practice.librarysystem.review;
 
+import com.practice.librarysystem.util.RequestConstants;
+import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import java.security.Principal;
-import jakarta.validation.Valid;
 
+import jakarta.servlet.http.HttpServletRequest;
+import java.security.Principal;
 import java.util.List;
 import java.util.stream.Collectors;
 
+@Slf4j
 @RestController
 @RequestMapping("/reviews")
 public class ReviewController {
@@ -23,37 +27,52 @@ public class ReviewController {
 
     @GetMapping
     public List<ReviewResponseDTO> getAll(@RequestParam(defaultValue = "0") int from,
-                                          @RequestParam(defaultValue = "10") int size) {
+                                          @RequestParam(defaultValue = "10") int size,
+                                          HttpServletRequest httpServletRequest) {
+        String ip = RequestConstants.getClientIp(httpServletRequest);
+        log.info("Endpoint GET: /reviews was accessed by IP:{}", ip);
+
         return reviewService.getAllReviews(from, size).stream()
                 .map(reviewMapper::toDTO)
                 .collect(Collectors.toList());
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<ReviewResponseDTO> getOne(@PathVariable Long id) {
+    public ResponseEntity<ReviewResponseDTO> getOne(@PathVariable Long id,
+                                                    HttpServletRequest httpServletRequest) {
+        String ip = RequestConstants.getClientIp(httpServletRequest);
+        log.info("Endpoint GET: /reviews/{} was accessed by IP:{}", id, ip);
+
         Review review = reviewService.getReview(id);
         return ResponseEntity.ok(reviewMapper.toDTO(review));
     }
 
+    @PostMapping
+    public ResponseEntity<ReviewResponseDTO> createReview(@RequestBody @Valid ReviewRequestDTO dto,
+                                                          HttpServletRequest httpServletRequest) {
+        String ip = RequestConstants.getClientIp(httpServletRequest);
+        log.info("Endpoint POST: /reviews was accessed by IP:{}", ip);
 
-   @PostMapping
-    public ResponseEntity<ReviewResponseDTO> createReview(@RequestBody @Valid ReviewRequestDTO dto) {
         Review review = reviewService.createReview(dto);
-        ReviewResponseDTO response = reviewMapper.toDTO(review);
-        return ResponseEntity.ok(response);
+        return ResponseEntity.ok(reviewMapper.toDTO(review));
     }
 
     @GetMapping("/book/{bookId}")
-    public List<ReviewResponseDTO> getReviewsByBook(@PathVariable Long bookId) {
+    public List<ReviewResponseDTO> getReviewsByBook(@PathVariable Long bookId,
+                                                    HttpServletRequest httpServletRequest) {
+        String ip = RequestConstants.getClientIp(httpServletRequest);
+        log.info("Endpoint GET: /reviews/book/{} was accessed by IP:{}", bookId, ip);
+
         return reviewService.getReviewsByBookId(bookId);
     }
 
-
     @PutMapping("/{id}")
-    public ResponseEntity<ReviewResponseDTO> update(
-            @PathVariable Long id,
-            @RequestBody ReviewRequestDTO dto,
-            Principal principal) {
+    public ResponseEntity<ReviewResponseDTO> update(@PathVariable Long id,
+                                                    @RequestBody ReviewRequestDTO dto,
+                                                    Principal principal,
+                                                    HttpServletRequest httpServletRequest) {
+        String ip = RequestConstants.getClientIp(httpServletRequest);
+        log.info("Endpoint PUT: /reviews/{} was accessed by IP:{} by user:{}", id, ip, principal.getName());
 
         Review updatedReview = reviewService.updateReview(id, dto, principal.getName());
         return ResponseEntity.ok(reviewMapper.toDTO(updatedReview));
@@ -61,9 +80,12 @@ public class ReviewController {
 
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void deleteReview(@PathVariable Long id, Principal principal) {
+    public void deleteReview(@PathVariable Long id,
+                             Principal principal,
+                             HttpServletRequest httpServletRequest) {
+        String ip = RequestConstants.getClientIp(httpServletRequest);
+        log.info("Endpoint DELETE: /reviews/{} was accessed by IP:{} by user:{}", id, ip, principal.getName());
+
         reviewService.deleteReview(id, principal.getName());
     }
-
 }
-
---
+++ src/main/java/com/practice/librarysystem/util/RequestConstants.java
@@ -0,0 +1,14 @@
+package com.practice.librarysystem.util;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+public class RequestConstants {
+
+    public static String getClientIp(HttpServletRequest request) {
+        String xfHeader = request.getHeader("X-Forwarded-For");
+        if (xfHeader == null || xfHeader.isEmpty()) {
+            return request.getRemoteAddr();
+        }
+        return xfHeader.split(",")[0];
+    }
+}
